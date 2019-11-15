5e68fa665e886b228807b943985cd135c2508a51
Kevin Pugliese <kevin.lima.contato@gmail.com>
2019-11-15 13:36:29 -0300

    initial

---
+++ src/main/java/com/kafka/kevin/Teste/TesteKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.kevin.Teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteKafkaApplication.class, args);
+	}
+
+}
